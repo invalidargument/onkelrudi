4e7b43f04d049ca6b1cd018f25fc3326f22549f3
Rudi Bieller <kontakt@rudi-bieller.de>
2015-11-14 21:07:48 +0000

    added mysql and liquibase

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # onkelrudi
 
 [![Build Status](https://travis-ci.org/invalidargument/onkelrudi.svg?branch=master)](https://travis-ci.org/invalidargument/onkelrudi)
+
+./vendor/alcaeus/liquibase/liquibase --defaultsFile=deployment/liquibase.properties update
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+add-apt-repository ppa:webupd8team/java
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root'
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 apt-get update
-apt-get install -y curl php5-common php5-cli php5-curl dh-make-php php5-dev git
+
+apt-get install -y curl php5-common php5-cli php5-curl dh-make-php php5-dev git ant
+apt-get install -y oracle-java8-installer
+apt-get install -y oracle-java8-set-default
 
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin/ --filename=composer
 
@@ -16,3 +24,6 @@ php5enmod xdebug
 
 chmod 0755 /vagrant/vendor/phpunit/phpunit/phpunit
 chmod 0755 /vagrant/vendor/behat/behat/bin/behat
+chmod 0755 /vagrant/vendor/alcaeus/liquibase/liquibase
+
+apt-get install -y mysql-server libmysql-java
