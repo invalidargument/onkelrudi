ed1005ec52be3555b776dad00054a7c1faf2cb3f
Rudi Bieller <kontakt@rudi-bieller.de>
2015-11-19 17:49:29 +0000

    added tests for factory

--- src/FleaMarket/Query/Factory.php
+++ src/FleaMarket/Query/Factory.php
@@ -4,17 +4,199 @@ namespace RudiBieller\OnkelRudi\FleaMarket\Query;
 
 class Factory
 {
-    private $_fleaMarketQuery;
+    private $_fleaMarketDeleteQuery;
+    private $_fleaMarketInsertQuery;
+    private $_fleaMarketReadListQuery;
+    private $_fleaMarketReadQuery;
+    private $_fleaMarketUpdateQuery;
+    private $_fleaMarketOrganizerDeleteQuery;
+    private $_fleaMarketOrganizerInsertQuery;
+    private $_fleaMarketOrganizerReadListQuery;
+    private $_fleaMarketOrganizerReadQuery;
+    private $_fleaMarketOrganizerUpdateQuery;
+    private $_fleaMarketDetailsDeleteQuery;
+    private $_fleaMarketDetailsInsertQuery;
+    private $_fleaMarketDetailsReadListQuery;
+    private $_fleaMarketDetailsReadQuery;
+    private $_fleaMarketDetailsUpdateQuery;
+
+    /**
+     * @return \RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketDeleteQuery
+     */
+    public function createFleaMarketDeleteQuery()
+    {
+        if (is_null($this->_fleaMarketDeleteQuery)) {
+            $this->_fleaMarketDeleteQuery = new FleaMarketDeleteQuery();
+        }
+
+        return $this->_fleaMarketDeleteQuery;
+    }
+
+    /**
+     * @return \RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketInsertQuery
+     */
+    public function createFleaMarketInsertQuery()
+    {
+        if (is_null($this->_fleaMarketInsertQuery)) {
+            $this->_fleaMarketInsertQuery = new FleaMarketInsertQuery();
+        }
+
+        return $this->_fleaMarketInsertQuery;
+    }
+
+    /**
+     * @return \RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketReadListQuery
+     */
+    public function createFleaMarketReadListQuery()
+    {
+        if (is_null($this->_fleaMarketReadListQuery)) {
+            $this->_fleaMarketReadListQuery = new FleaMarketReadListQuery();
+        }
+
+        return $this->_fleaMarketReadListQuery;
+    }
+
+    /**
+     * @return \RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketReadQuery
+     */
+    public function createFleaMarketReadQuery()
+    {
+        if (is_null($this->_fleaMarketReadQuery)) {
+            $this->_fleaMarketReadQuery = new FleaMarketReadQuery();
+        }
+
+        return $this->_fleaMarketReadQuery;
+    }
+
+    /**
+     * @return \RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketUpdateQuery
+     */
+    public function createFleaMarketUpdateQuery()
+    {
+        if (is_null($this->_fleaMarketUpdateQuery)) {
+            $this->_fleaMarketUpdateQuery = new FleaMarketUpdateQuery();
+        }
+
+        return $this->_fleaMarketUpdateQuery;
+    }
+
+    /**
+     * @return \RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketOrganizerDeleteQuery
+     */
+    public function createFleaMarketOrganizerDeleteQuery()
+    {
+        if (is_null($this->_fleaMarketOrganizerDeleteQuery)) {
+            $this->_fleaMarketOrganizerDeleteQuery = new FleaMarketOrganizerDeleteQuery();
+        }
+
+        return $this->_fleaMarketOrganizerDeleteQuery;
+    }
 
     /**
      * @return \RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketOrganizerInsertQuery
      */
-    public function createFleaMarketQuery()
+    public function createFleaMarketOrganizerInsertQuery()
     {
-        if (is_null($this->_fleaMarketQuery)) {
-            $this->_fleaMarketQuery = new FleaMarketOrganizerInsertQuery();
+        if (is_null($this->_fleaMarketOrganizerInsertQuery)) {
+            $this->_fleaMarketOrganizerInsertQuery = new FleaMarketOrganizerInsertQuery();
         }
 
-        return $this->_fleaMarketQuery;
+        return $this->_fleaMarketOrganizerInsertQuery;
+    }
+
+    /**
+     * @return \RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketOrganizerReadListQuery
+     */
+    public function createFleaMarketOrganizerReadListQuery()
+    {
+        if (is_null($this->_fleaMarketOrganizerReadListQuery)) {
+            $this->_fleaMarketOrganizerReadListQuery = new FleaMarketOrganizerReadListQuery();
+        }
+
+        return $this->_fleaMarketOrganizerReadListQuery;
+    }
+
+    /**
+     * @return \RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketOrganizerReadQuery
+     */
+    public function createFleaMarketOrganizerReadQuery()
+    {
+        if (is_null($this->_fleaMarketOrganizerReadQuery)) {
+            $this->_fleaMarketOrganizerReadQuery = new FleaMarketOrganizerReadQuery();
+        }
+
+        return $this->_fleaMarketOrganizerReadQuery;
+    }
+
+    /**
+     * @return \RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketOrganizerUpdateQuery
+     */
+    public function createFleaMarketOrganizerUpdateQuery()
+    {
+        if (is_null($this->_fleaMarketOrganizerUpdateQuery)) {
+            $this->_fleaMarketOrganizerUpdateQuery = new FleaMarketOrganizerUpdateQuery();
+        }
+
+        return $this->_fleaMarketOrganizerUpdateQuery;
+    }
+
+    /**
+     * @return \RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketDetailsDeleteQuery
+     */
+    public function createFleaMarketDetailsDeleteQuery()
+    {
+        if (is_null($this->_fleaMarketDetailsDeleteQuery)) {
+            $this->_fleaMarketDetailsDeleteQuery = new FleaMarketDetailsDeleteQuery();
+        }
+
+        return $this->_fleaMarketDetailsDeleteQuery;
+    }
+
+    /**
+     * @return \RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketDetailsInsertQuery
+     */
+    public function createFleaMarketDetailsInsertQuery()
+    {
+        if (is_null($this->_fleaMarketDetailsInsertQuery)) {
+            $this->_fleaMarketDetailsInsertQuery = new FleaMarketDetailsInsertQuery();
+        }
+
+        return $this->_fleaMarketDetailsInsertQuery;
+    }
+
+    /**
+     * @return \RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketDetailsReadListQuery
+     */
+    public function createFleaMarketDetailsReadListQuery()
+    {
+        if (is_null($this->_fleaMarketDetailsReadListQuery)) {
+            $this->_fleaMarketDetailsReadListQuery = new FleaMarketDetailsReadListQuery();
+        }
+
+        return $this->_fleaMarketDetailsReadListQuery;
+    }
+
+    /**
+     * @return \RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketDetailsReadQuery
+     */
+    public function createFleaMarketDetailsReadQuery()
+    {
+        if (is_null($this->_fleaMarketDetailsReadQuery)) {
+            $this->_fleaMarketDetailsReadQuery = new FleaMarketDetailsReadQuery();
+        }
+
+        return $this->_fleaMarketDetailsReadQuery;
+    }
+
+    /**
+     * @return \RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketDetailsUpdateQuery
+     */
+    public function createFleaMarketDetailsUpdateQuery()
+    {
+        if (is_null($this->_fleaMarketDetailsUpdateQuery)) {
+            $this->_fleaMarketDetailsUpdateQuery = new FleaMarketDetailsUpdateQuery();
+        }
+
+        return $this->_fleaMarketDetailsUpdateQuery;
     }
 }
--- tests/FleaMarket/Query/FactoryTest.php
+++ tests/FleaMarket/Query/FactoryTest.php
@@ -11,12 +11,152 @@ class FactoryTest extends \PHPUnit_Framework_TestCase
         $this->_sut = new Factory();
     }
 
-    public function testFactoryCreatesFleaMarketQuery()
+    public function testFactoryCreatesFleaMarketDeleteQuery()
     {
-        $query = $this->_sut->createFleaMarketQuery();
+        $query = $this->_sut->createFleaMarketDeleteQuery();
+
+        $this->assertInstanceOf(
+            'RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketDeleteQuery',
+            $query
+        );
+    }
+
+    public function testFactoryCreatesFleaMarketInsertQuery()
+    {
+        $query = $this->_sut->createFleaMarketInsertQuery();
+
+        $this->assertInstanceOf(
+            'RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketInsertQuery',
+            $query
+        );
+    }
+
+    public function testFactoryCreatesFleaMarketReadListQuery()
+    {
+        $query = $this->_sut->createFleaMarketReadListQuery();
+
+        $this->assertInstanceOf(
+            'RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketReadListQuery',
+            $query
+        );
+    }
+
+    public function testFactoryCreatesFleaMarketReadQuery()
+    {
+        $query = $this->_sut->createFleaMarketReadQuery();
+
+        $this->assertInstanceOf(
+            'RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketReadQuery',
+            $query
+        );
+    }
+
+    public function testFactoryCreatesFleaMarketUpdateQuery()
+    {
+        $query = $this->_sut->createFleaMarketUpdateQuery();
+
+        $this->assertInstanceOf(
+            'RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketUpdateQuery',
+            $query
+        );
+    }
+
+    public function testFactoryCreatesFleaMarketOrganizerDeleteQuery()
+    {
+        $query = $this->_sut->createFleaMarketOrganizerDeleteQuery();
+
+        $this->assertInstanceOf(
+            'RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketOrganizerDeleteQuery',
+            $query
+        );
+    }
+
+    public function testFactoryCreatesFleaMarketOrganizerInsertQuery()
+    {
+        $query = $this->_sut->createFleaMarketOrganizerInsertQuery();
 
         $this->assertInstanceOf(
             'RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketOrganizerInsertQuery',
+            $query
+        );
+    }
+
+    public function testFactoryCreatesFleaMarketOrganizerReadListQuery()
+    {
+        $query = $this->_sut->createFleaMarketOrganizerReadListQuery();
+
+        $this->assertInstanceOf(
+            'RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketOrganizerReadListQuery',
+            $query
+        );
+    }
+
+    public function testFactoryCreatesFleaOrganizerMarketReadQuery()
+    {
+        $query = $this->_sut->createFleaMarketOrganizerReadQuery();
+
+        $this->assertInstanceOf(
+            'RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketOrganizerReadQuery',
+            $query
+        );
+    }
+
+    public function testFactoryCreatesFleaMarketOrganizerUpdateQuery()
+    {
+        $query = $this->_sut->createFleaMarketOrganizerUpdateQuery();
+
+        $this->assertInstanceOf(
+            'RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketOrganizerUpdateQuery',
+            $query
+        );
+    }
+
+    public function testFactoryCreatesFleaMarketDetailsDeleteQuery()
+    {
+        $query = $this->_sut->createFleaMarketDetailsDeleteQuery();
+
+        $this->assertInstanceOf(
+            'RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketDetailsDeleteQuery',
+            $query
+        );
+    }
+
+    public function testFactoryCreatesFleaMarketDetailsInsertQuery()
+    {
+        $query = $this->_sut->createFleaMarketDetailsInsertQuery();
+
+        $this->assertInstanceOf(
+            'RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketDetailsInsertQuery',
+            $query
+        );
+    }
+
+    public function testFactoryCreatesFleaMarketDetailsReadListQuery()
+    {
+        $query = $this->_sut->createFleaMarketDetailsReadListQuery();
+
+        $this->assertInstanceOf(
+            'RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketDetailsReadListQuery',
+            $query
+        );
+    }
+
+    public function testFactoryCreatesFleaMarketDetailsReadQuery()
+    {
+        $query = $this->_sut->createFleaMarketDetailsReadQuery();
+
+        $this->assertInstanceOf(
+            'RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketDetailsReadQuery',
+            $query
+        );
+    }
+
+    public function testFactoryCreatesFleaMarketDetailsUpdateQuery()
+    {
+        $query = $this->_sut->createFleaMarketDetailsUpdateQuery();
+
+        $this->assertInstanceOf(
+            'RudiBieller\OnkelRudi\FleaMarket\Query\FleaMarketDetailsUpdateQuery',
             $query
         );
     }
