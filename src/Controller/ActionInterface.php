58424b223532ab25d1fc403b783e7884000958a1
Rudi Bieller <kontakt@rudi-bieller.de>
2015-12-17 12:49:12 +0000

    added BuilderFactory

--- src/Controller/ActionInterface.php
+++ src/Controller/ActionInterface.php
@@ -2,6 +2,7 @@
 
 namespace RudiBieller\OnkelRudi\Controller;
 
+use RudiBieller\OnkelRudi\BuilderFactoryInterface;
 use RudiBieller\OnkelRudi\ServiceInterface;
 
 interface ActionInterface
@@ -9,4 +10,6 @@ interface ActionInterface
     public function setApp(\Slim\App $app);
 
     public function setService(ServiceInterface $service);
+
+    public function setBuilderFactory(BuilderFactoryInterface $factory);
 }
