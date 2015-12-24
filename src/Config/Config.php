b1bb7b433e7af5f32bb482aa62b474d77777e79e
Rudi Bieller <kontakt@rudi-bieller.de>
2015-12-24 01:58:27 +0000

    Config reading from yaml now

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -1,9 +1,40 @@
 <?php
 namespace RudiBieller\OnkelRudi\Config;
 
+use Symfony\Component\Yaml\Yaml;
+
 class Config
 {
-    public static $dsn = 'mysql:host=127.0.0.1;dbname=onkelrudi;charset=utf8';
-    public static $user = 'root';
-    public static $password = 'root';
+    private static $_parsedConfig;
+
+    public function __construct()
+    {
+        $this->_parseConfig();
+    }
+
+    public function getConfiguration()
+    {
+        return self::$_parsedConfig;
+    }
+
+    public function getDatabaseConfiguration()
+    {
+        return self::$_parsedConfig['Database'];
+    }
+
+    public function getSystemConfiguration()
+    {
+        return self::$_parsedConfig['System'];
+    }
+
+    private function _parseConfig()
+    {
+        if (is_null(self::$_parsedConfig)) {
+            $configFile = dirname(__FILE__) . '/../../deployment/settings.yml';
+            $yamlParser = new Yaml();
+            self::$_parsedConfig = $yamlParser->parse(file_get_contents($configFile));
+        }
+
+        return self::$_parsedConfig;
+    }
 }
---
+++ tests/Config/ConfigTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace RudiBieller\OnkelRudi\Config;
+
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
+    private $_sut;
+
+    protected function setUp()
+    {
+        $this->_sut = new Config();
+    }
+
+    public function testConfigReturnsParsedConfiguration()
+    {
+        $result = $this->_sut->getConfiguration();
+
+        $this->assertArrayHasKey('Database', $result);
+        $this->assertArrayHasKey('System', $result);
+    }
+
+    public function testConfigReturnsParsedDatabaseConfiguration()
+    {
+        $result = $this->_sut->getDatabaseConfiguration();
+
+        $this->assertArrayHasKey('dsn', $result);
+        $this->assertArrayHasKey('user', $result);
+        $this->assertArrayHasKey('password', $result);
+    }
+
+    public function testConfigReturnsParsedSystemConfiguration()
+    {
+        $result = $this->_sut->getSystemConfiguration();
+
+        $this->assertArrayHasKey('environment', $result);
+    }
+}
