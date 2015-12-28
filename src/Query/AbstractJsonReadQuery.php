87b68c34938967c491acc6ba40bb889f11e6944c
Rudi Bieller <kontakt@rudi-bieller.de>
2015-12-28 22:12:10 +0000

    added AbstractJsonReadQuery

---
+++ src/Query/AbstractJsonReadQuery.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace RudiBieller\OnkelRudi\Query;
+
+abstract class AbstractJsonReadQuery implements QueryInterface
+{
+    protected $uri;
+    protected $browser;
+
+    abstract protected function mapResult($result);
+
+    /**
+     * @return string
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
+
+    /**
+     * @param string $uri
+     */
+    public function setUri($uri)
+    {
+        $this->uri = $uri;
+        return $this;
+    }
+
+    public function run()
+    {
+        return $this->mapResult($this->runQuery());
+    }
+
+    protected function runQuery()
+    {
+        $curl = new \Buzz\Client\Curl();
+        $curl->setOption(CURLOPT_FOLLOWLOCATION, false);
+        $this->browser = new \Buzz\Browser($curl);
+
+        $url = $this->getUri();
+        $headers = array('Content-Type: application/json');
+        $body = '';
+
+        return $this->browser->get($url, $headers, $body);
+    }
+}
