3f9eb2ed0c4c33ecf83b2f9f6942f14d48a27941
Rudi Bieller <kontakt@rudi-bieller.de>
2016-01-11 18:09:16 +0000

    added orderBy

--- src/FleaMarket/Query/FleaMarketOrganizerReadListQuery.php
+++ src/FleaMarket/Query/FleaMarketOrganizerReadListQuery.php
@@ -28,6 +28,7 @@ class FleaMarketOrganizerReadListQuery extends AbstractQuery
         $selectStatement = $this->pdo
             ->select()
             ->from('fleamarkets_organizer')
+            ->orderBy('name', 'ASC')
             ->limit($this->_limit)
             ->offset($this->_offset);
 
