657956febf7e2551741eec09a6cb5d8a542a6dc5
Rudi Bieller <kontakt@rudi-bieller.de>
2016-03-25 17:24:44 +0000

    .

---
+++ src/User/DbAuthenticationAdapter.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace RudiBieller\OnkelRudi\User;
+
+use RudiBieller\OnkelRudi\Query\AbstractQuery;
+use Zend\Authentication\Adapter\AdapterInterface;
+use Zend\Authentication\Result;
+
+class DbAuthenticationAdapter extends AbstractQuery implements AdapterInterface
+{
+    private $_identifier;
+    private $_password;
+
+    /**
+     * @param string $identifier
+     * @return $this
+     */
+    public function setIdentifier($identifier)
+    {
+        $this->_identifier = $identifier;
+        return $this;
+    }
+
+    /**
+     * @param string $password
+     * @return $this
+     */
+    public function setPassword($password)
+    {
+        $this->_password = $password;
+        return $this;
+    }
+
+    public function authenticate()
+    {
+        $hash = $this->runQuery();
+
+        return $this->mapResult($hash);
+    }
+
+    protected function mapResult($hash)
+    {
+        if (!$hash) {
+            return new Result(Result::FAILURE_IDENTITY_NOT_FOUND, $this->_identifier);
+        }
+
+        if (password_verify($this->_password, $hash)) {
+            return new Result(Result::SUCCESS, $this->_identifier);
+        }
+
+        return new Result(Result::FAILURE_CREDENTIAL_INVALID, $this->_identifier);
+    }
+
+    protected function runQuery()
+    {
+        $selectStatement = $this->getPdo()
+            ->select()
+            ->from('fleamarkets_users')
+            ->where('email', '=', $this->_identifier);
+
+        /**
+         * @var \Slim\PDO\Statement
+         */
+        $statement = $selectStatement->execute();
+
+        return $statement->fetch(\PDO::FETCH_COLUMN);
+    }
+}
---
+++ tests/User/DbAuthenticationAdapterTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace RudiBieller\OnkelRudi\User;
+
+class DbAuthenticationAdapterTest extends \PHPUnit_Framework_TestCase
+{
+    private $_sut;
+    private $_pdo;
+
+    protected function setUp()
+    {
+        $this->_sut = new DbAuthenticationAdapter();
+        $this->_pdo = \Mockery::mock('\Slim\PDO\Database');
+        $this->_sut->setPdo($this->_pdo);
+    }
+
+    /**
+     * @dataProvider dataProviderTestAdapterVerifiesCredentialsByQueryingDatabase
+     */
+    public function testAdapterVerifiesCredentialsByQueryingDatabase($identifier, $password, $hash, $expectedResult, $expectedCode)
+    {
+        $this->_sut->setIdentifier($identifier)->setPassword($password);
+
+        $statement = \Mockery::mock('\PDOStatement');
+        $statement->shouldReceive('fetch')
+            ->once()
+            ->andReturn($hash);
+
+        $this->_pdo
+            ->shouldReceive('select')
+                ->andReturn($this->_pdo)
+            ->shouldReceive('from')
+                ->with('fleamarkets_users')
+                ->andReturn($this->_pdo)
+            ->shouldReceive('where')
+                ->with('email', '=', 'foo@bar.de')
+                ->andReturn($this->_pdo)
+            ->shouldReceive('execute')
+                ->once()
+                ->andReturn($statement);
+
+        $result = $this->_sut->authenticate();
+
+        $this->assertInstanceOf('Zend\Authentication\Result', $result);
+        $this->assertSame($expectedResult, $result->isValid());
+        $this->assertSame($expectedCode, $result->getCode());
+    }
+
+    public function dataProviderTestAdapterVerifiesCredentialsByQueryingDatabase()
+    {
+        return array(
+            array('foo@bar.de', '123', password_hash('123', PASSWORD_DEFAULT), true, 1),
+            array('foo@bar.de', '123', password_hash('666', PASSWORD_DEFAULT), false, -3),
+            array('foo@bar.de', '123', null, false, -1)
+        );
+    }
+}
