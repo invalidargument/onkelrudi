3e71462376e5f44ddace56eb252041fe8e4b1976
Rudi Bieller <kontakt@rudi-bieller.de>
2016-04-05 20:22:32 +0000

    User immutable

--- src/User/User.php
+++ src/User/User.php
@@ -2,16 +2,24 @@
 
 namespace RudiBieller\OnkelRudi\User;
 
-class User implements UserInterface, \JsonSerializable
+use RudiBieller\OnkelRudi\ImmutableInterface;
+
+class User implements UserInterface, ImmutableInterface, \JsonSerializable
 {
     private $_identifier;
     private $_password;
     private $_type;
 
-    public function __construct($identifier = null, $password = null)
+    public function __construct($identifier = null, $password = null, $type = UserInterface::TYPE_USER)
     {
         $this->_identifier = $identifier;
         $this->_password = $password;
+
+        if (!in_array($type, array(self::TYPE_USER, self::TYPE_ADMIN))) {
+            throw new \InvalidArgumentException('Invalid usertype given, allowed are ' . self::TYPE_USER . ' and ' . self::TYPE_ADMIN);
+        }
+
+        $this->_type = $type;
     }
 
     /**
@@ -23,16 +31,6 @@ class User implements UserInterface, \JsonSerializable
     }
 
     /**
-     * @param string $identifier
-     * @return User
-     */
-    public function setIdentifier($identifier)
-    {
-        $this->_identifier = $identifier;
-        return $this;
-    }
-
-    /**
      * @return string
      */
     public function getPassword()
@@ -41,36 +39,11 @@ class User implements UserInterface, \JsonSerializable
     }
 
     /**
-     * @param string $password
-     * @return User
-     */
-    public function setPassword($password)
-    {
-        $this->_password = $password;
-        return $this;
-    }
-
-    /**
      * @return string
      */
     public function getType()
     {
         return $this->_type;
-    }
-
-    /**
-     * @param string $type
-     * @return User
-     * @throws \InvalidArgumentException
-     */
-    public function setType($type)
-    {
-        if (!in_array($type, array(self::TYPE_USER, self::TYPE_ADMIN))) {
-            throw new \InvalidArgumentException('Invalid usertype given, allowed are ' . self::TYPE_USER . ' and ' . self::TYPE_ADMIN);
-        }
-
-        $this->_type = $type;
-        return $this;
     }
 
     public function jsonSerialize()
