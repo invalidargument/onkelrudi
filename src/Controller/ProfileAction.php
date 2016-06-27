05d872dbfa73e1e8cbca0a7a35f2f11f6ef294c8
Rudi Bieller <kontakt@rudi-bieller.de>
2016-06-27 20:33:01 +0000

    added option to create new market right away

--- src/Controller/ProfileAction.php
+++ src/Controller/ProfileAction.php
@@ -9,6 +9,7 @@ class ProfileAction extends AbstractHttpAction implements UserAwareInterface
     protected function getData()
     {
         $this->templateVariables['profileurl'] = $this->app->getContainer()->get('router')->pathFor('login-register');
+        $this->templateVariables['createfleamarketurl'] = $this->app->getContainer()->get('router')->pathFor('create-fleamarket');
 
         return $this->userService->getAuthenticationService()->getStorage()->read();
     }
