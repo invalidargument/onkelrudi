a4b83f05b326332bce31a5278eb7f03bb5aa773d
Rudi Bieller <kontakt@rudi-bieller.de>
2016-10-16 19:28:39 +0000

    fix display of organizer fields in edit

--- src/Controller/EditFleaMarketAction.php
+++ src/Controller/EditFleaMarketAction.php
@@ -2,7 +2,7 @@
 
 namespace RudiBieller\OnkelRudi\Controller;
 
-use RudiBieller\OnkelRudi\FleaMarket\FleaMarketInterface;
+use RudiBieller\OnkelRudi\User\Organizer;
 
 class EditFleaMarketAction extends AbstractHttpAction implements UserAwareInterface
 {
@@ -13,11 +13,18 @@ class EditFleaMarketAction extends AbstractHttpAction implements UserAwareInterf
         $fleamarketId = $this->args['id'];
         $fleamarket = $this->service->getFleaMarket($fleamarketId);
 
+        $organizer = null;
+        $user = $this->userService->getAuthenticationService()->getStorage()->read();
+        if ($user instanceof Organizer) {
+            $organizer = $this->organizerService->getOrganizerByUserId($user->getIdentifier());
+        }
+
         $this->templateVariables['isTest'] = $this->isTestRequest;
         $this->templateVariables['loggedIn'] = true;
         $this->templateVariables['fleamarket_organizers'] = [];
         $this->templateVariables['editForm'] = true;
         $this->templateVariables['editDto'] = $fleamarket;
+        $this->templateVariables['organizer'] = $organizer;
         return [];
     }
 }
