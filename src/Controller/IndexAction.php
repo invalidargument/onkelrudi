5676d7a47c15846331c325eb443f3b595700e094
Rudi Bieller <kontakt@rudi-bieller.de>
2016-11-06 22:30:15 +0000

    pmd

--- src/Controller/IndexAction.php
+++ src/Controller/IndexAction.php
@@ -11,18 +11,14 @@ class IndexAction extends AbstractHttpAction
     protected function getData()
     {
         $start = null;
-        $monthArgument = array_key_exists('month', $this->args)
-            ? $this->args['month']
-            : null;
+        $monthArgument = $this->_getArgValue('month');
 
         if (!is_null($monthArgument)) {
             list($month, $year) = explode('-', $monthArgument);
             $start = new \DateTimeImmutable($year . '-' . $month . '-01 00:00:01');
         }
 
-        $zip = array_key_exists('zip', $this->args)
-            ? $this->args['zip']
-            : self::DEFAULT_ZIP_AREA;
+        $zipArgument = $this->_getArgValue('zip');
 
         $isTest = strpos($this->request->getUri()->getQuery(), 'test=1') !== false;
         $fleaMarkets = $this->service->getAllFleaMarketsByTimespan($start);
@@ -54,21 +50,28 @@ class IndexAction extends AbstractHttpAction
         $this->templateVariables['monthRange'] = $monthRange;
         $this->templateVariables['zipAreaRange'] = $zipAreaRange;
         $this->templateVariables['selectedMonth'] = str_replace('-', '/', $monthArgument);
-        $this->templateVariables['selectedZipAreaRange'] = $this->_getSelectedZipRange($zip);
+        $this->templateVariables['selectedZipAreaRange'] = $this->_getSelectedZipRange($zipArgument);
         $this->templateVariables['isLoggedIn'] = $this->userService->isLoggedIn();
         $this->templateVariables['isTest'] = (boolean)$isTest;
-
-        //var_dump($this->templateVariables);die;
 
         return array();
     }
 
     private function _getSelectedZipRange($zip)
     {
-        if ($zip == self::DEFAULT_ZIP_AREA) {
+        if (is_null($zip) || $zip == self::DEFAULT_ZIP_AREA) {
             return self::DEFAULT_ZIP_AREA;
         }
 
         return substr($zip, 0, 1);
+    }
+
+    private function _getArgValue($name)
+    {
+        if (array_key_exists($name, $this->args)) {
+            return $this->args[$name];
+        }
+
+        return null;
     }
 }
