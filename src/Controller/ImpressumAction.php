37db4070c5d80b70cf39113db538109f939e4fe7
Rudi Bieller <kontakt@rudi-bieller.de>
2017-09-17 21:19:12 +0000

    added codeCoverageIgnore

--- src/Controller/ImpressumAction.php
+++ src/Controller/ImpressumAction.php
@@ -4,6 +4,9 @@ namespace RudiBieller\OnkelRudi\Controller;
 
 use RudiBieller\OnkelRudi\CacheableInterface;
 
+/**
+ * @codeCoverageIgnore
+ */
 class ImpressumAction extends AbstractHttpAction implements CacheableInterface
 {
     protected $template = 'impressum.html';
